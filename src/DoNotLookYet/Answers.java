c72beb63d6627ad9aa1ad2fff31f2a1efcff96ea
Omar <osahtout@hotmail.com>
2023-03-06 21:43:21 -0500

    completed

--- src/DoNotLookYet/Answers.java
+++ src/DoNotLookYet/Answers.java
@@ -6,52 +6,81 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.function.IntFunction;
 import java.util.stream.Collectors;
 
 public class Answers
 {
-//	public static List<String> getUniqueCompanies(List<Employee> employees)
-//	{
-//		return employees.stream().map(employee -> employee.getCompany()).distinct().collect(Collectors.toList());
-//	}
-//
-//	public static void streamFilterMapForEachTest(List<String> list)
-//	{
-//		list.stream()
-//				.filter(string -> string.length() > 3)
-//				.map(string -> string.toUpperCase())
-//				.forEach(string -> System.out.println(string));
-//
-//	}
-//
-//	public static List<Integer> testFilter(List<Integer> list)
-//	{
-//		return list.stream().filter(n -> isEven.apply(n)).collect(Collectors.toList());
-//	}
-//
-//	public static void testVarargs(String string, int... a)
-//	{
-//		System.out.println(string + Arrays.stream(a).sum());
-//	}
-//
-//	public static Function<Integer, Boolean> isEven = (number) -> number % 2 == 0;
-//
-//
-//	public static BiFunction<String, List<Character>, String> concat = (string, chars) -> helperFunction(string, chars);
-//
-//	private static String helperFunction(String string, List<Character> chars)
-//	{
-//		String upperCase = string.toUpperCase();
-//		StringBuilder sb = new StringBuilder();
-//
-//		sb.append(upperCase);
-//
-//		for ( Character ch: chars)
-//		{
-//			sb.append(ch);
-//		}
-//
-//		return sb.toString();
-//	}
+	public static List<String> getUniqueCompanies(List<Employee> employees)
+	{
+		return employees.stream().map(employee -> employee.getCompany()).distinct().collect(Collectors.toList());
+	}
+
+	public static List<String> streamFilterMapForEachTest(List<String> list)
+	{
+		return list.stream()
+				.filter(string -> string.length() > 3)
+				.map(string -> string.toUpperCase())
+				.collect(Collectors.toList());
+
+	}
+
+	public static List<Integer> testFilter(List<Integer> list)
+	{
+		return list.stream().filter(n -> isEven.apply(n)).collect(Collectors.toList());
+	}
+
+	public static String testVarargs(String string, int... a)
+	{
+		return (string + Arrays.stream(a).sum());
+	}
+
+	public static Function<Integer, Boolean> isEven = (number) -> number % 2 == 0;
+
+
+	public static BiFunction<String, List<Character>, String> concat = (string, chars) -> helperFunction(string, chars);
+
+	private static String helperFunction(String string, List<Character> chars)
+	{
+		String upperCase = string.toUpperCase();
+		StringBuilder sb = new StringBuilder();
+
+		sb.append(upperCase);
+
+		for ( Character ch: chars)
+		{
+			sb.append(ch);
+		}
+
+		return sb.toString();
+	}
+
+
+	public static IntFunction<String> helloworld = (number) -> {
+
+		if(number < 0)
+		{
+			return "-1";
+		}
+
+		String thestring = "Hello, world!";
+		if(number <= thestring.length())
+		{
+			return thestring.substring(0, number);
+		}
+
+		if(number > thestring.length())
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.append(thestring);
+			for (int i = 0; i < number; i++)
+			{
+				sb.append("!");
+			}
+			return sb.toString();
+		}
+
+		return "-1";
+	};
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,114 +2,335 @@ import Company.Employee;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.BiFunction;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 //import static DoNotLookYet.Answers.getUniqueCompanies;
 //import static DoNotLookYet.Answers.isEven;
 //import static DoNotLookYet.Answers.concat;
+//import static DoNotLookYet.Answers.helloworld;
 //import static DoNotLookYet.Answers.streamFilterMapForEachTest;
 //import static DoNotLookYet.Answers.testFilter;
 //import static DoNotLookYet.Answers.testVarargs;
 
 public class Main
 {
-	public static void main(String[] args)
+
+
+	/** WRITE YOUR STATIC FUNCTION HERE **/
+
+	public static void main(String[] args) throws Exception
 	{
 
-		/**
+
+		/**  1.0
 		 * Can you write a Functional interface that takes an int and returns if isEven?
 		 * Below should print false
 		 *
 		 */
-		System.out.println(isEven.apply(999));
+
+//		if(Boolean.TRUE.equals(isEven.apply(999))) {
+//			throw (new Exception(getErrorMessage(1)));
+//		}
+//		else{
+//			System.out.println(getCompletedMessage(1));
+//		}
 
 
 
-		/**
+
+
+
+
+
+
+		/** ============================================================================= */
+		/** ============================================================================= */
+		/** ============================================================================= */
+
+
+
+
+
+
+
+
+
+
+
+		/**  2.0
 		 * Write a function interface called concat that takes a string and an array of characters,
 		 * upper case the string,
 		 * concatenate all the characters
 		 * concatenate the string and the concatenated list
 		 * return the new string
-		 * Should print HELLO, world
+		 * Should return HELLO, world
 		 */
 
-		System.out.println(concat.apply("hello, ", List.of('w', 'o', 'r', 'l', 'd')));
+
+//		if(!concat.apply("hello, ", List.of('w', 'o', 'r', 'l', 'd')).equals("HELLO, world")) {
+//			throw (new Exception(getErrorMessage(2)));
+//		}
+//		else {
+//			System.out.println(getCompletedMessage(2));
+//		}
 
 
 
 		/**###########################################################################################
 
-		/**** java.util.function has many functional interfaces that can be used in the future if needed ****/
-
-		/**##########################################################################################
-
-
+		/** java.util.function has many functional interfaces that takes one or two object and returns an object */
 
 		/**
+		 * some have predefined return objects to make it simple for developers.
+		 * Predicate takes an objects and returns a boolean. We will see this in filters
+		 *
+		 * Other takes a certain primitive object like int and returns an object of preference
+		 * IntFunction needs to specify the return object but always takes an int and return the object specified
+
+		/**########################################################################################## */
+
+
+
+		/** 3.0
+		 * use IntFunction that takes a number and return Hello, world! up to the number specified where h is 1.
+		 * 3.1 if 3 is specified, it should return Hel
+		 * 3.2 if the user specify a negative number, return "-1"
+		 * 3.3 if the user specifies a number longer than the characters available add ! until the number is reached
+		 */
+
+//		if(!helloworld.apply(3).equals("Hel"))
+//		{
+//			throw (new Exception(getErrorMessage(3.1)));
+//		}
+//		else {
+//			System.out.println(getCompletedMessage(3.1));
+//		}
+//
+//		if(!helloworld.apply(-1).equals("-1"))
+//		{
+//			throw (new Exception(getErrorMessage(3.2)));
+//		}
+//		else {
+//			System.out.println(getCompletedMessage(3.2));
+//		}
+//
+//		if(!helloworld.apply(20).equals("Hello, world!!!!!!!!!!!!!!!!!!!!!"))
+//		{
+//			throw (new Exception(getErrorMessage(3.3)));
+//		}
+//		else {
+//			System.out.println(getCompletedMessage(3.3));
+//		}
+
+
+
+
+
+
+
+
+
+
+		/** ============================================================================= */
+		/** ============================================================================= */
+		/** ============================================================================= */
+
+
+
+
+
+
+
+
+
+
+
+
+		/** 4.0
 		 * Given this array of integers, write a function called testFilter that
 		 * filter out the odd numbers using isEven from the previous example
 		 */
-		testFilter(Arrays.asList(1, 2, 2, 32, 3, 23, 23, 31, 231, 5234, 63745 ));
+
+//		final List<Integer> filtered = testFilter(Arrays.asList(1, 2, 2, 32, 3, 23, 23, 31, 231, 5234, 63745));
+//
+//		if(Arrays.equals(expected.toArray(), filtered.toArray()))
+//		{
+//			System.out.println(getCompletedMessage(4));
+//		}
+//		else
+//		{
+//			throw (new Exception(getErrorMessage(4)));
+//		}
 
 
-		/**
+
+
+
+
+
+
+
+		/** ============================================================================= */
+		/** ============================================================================= */
+		/** ============================================================================= */
+
+
+
+
+
+
+
+
+
+
+		/** 5.0
 		 * using the stream, filter, map and foreach function, remove all the words shorter or equal to 3 characters,
-		 * convert the rest to upper case and print it out with a function called streamFilterMapForEachTest
-		 * Should print:
-		 * MOON
-		 * PLANET
-		 * JUPITER
-		 * SATURN
-		 * ANDROMIDA
-		 * NEPTUNE
-		 * PLUTO
+		 * convert the rest to upper case and return it out with a function called streamFilterMapForEachTest
 		 */
 
-		streamFilterMapForEachTest(List.of("moon", "sun", "planet", "eye", "s", "jupiter", "saturn", "andromida", "neptune", "pluto", "a"));
+//		final List<String> manipulatedString = streamFilterMapForEachTest(
+//				List.of("moon", "sun", "planet", "eye", "s", "jupiter", "saturn", "andromida", "neptune", "pluto", "a"));
+//
+//		if(Arrays.equals(expectedList.toArray(), manipulatedString.toArray()))
+//		{
+//			System.out.println(getCompletedMessage(5));
+//		}
+//		else
+//		{
+//			throw (new Exception(getErrorMessage(5)));
+//		}
 
-		/**
+
+
+
+
+
+
+		/** ============================================================================= */
+		/** ============================================================================= */
+		/** ============================================================================= */
+
+
+
+
+
+
+
+
+
+
+
+		/** 6.0
 		 * Write a static function that return a list of unique companies and print it out
-		 * Should print
-		 * SAP
-		 * Apple
-		 * Microsoft
-		 * Amazon
 		 */
+//		List<Employee> employees = List.of(
+//				new Employee("Bob", sap),
+//				new Employee("Joe", apple),
+//				new Employee("Joe", microsoft),
+//				new Employee("Joe", microsoft),
+//				new Employee("Joe", amazon),
+//				new Employee("Joe", apple),
+//				new Employee("Bob", sap),
+//				new Employee("Joe", apple),
+//				new Employee("Joe", amazon),
+//				new Employee("Bob", sap),
+//				new Employee("Joe", apple),
+//				new Employee("Joe", sap),
+//				new Employee("Joe", amazon),
+//				new Employee("Joe", apple),
+//				new Employee("Joe", microsoft));
+//
+//		final List<String> uniqueCompanies = getUniqueCompanies(employees);
+//
+//		if(Arrays.equals(expectedUniqueCompanies.toArray(), uniqueCompanies.toArray()))
+//		{
+//			System.out.println(getCompletedMessage(6));
+//		}
+//		else
+//		{
+//			throw (new Exception(getErrorMessage(5)));
+//		}
 
-		String sap = "SAP"; String apple = "Apple"; String microsoft = "Microsoft"; String amazon = "Amazon";
-		List<Employee> employees = List.of(
-				new Employee("Bob", sap),
-				new Employee("Joe", apple),
-				new Employee("Joe", microsoft),
-				new Employee("Joe", microsoft),
-				new Employee("Joe", amazon),
-				new Employee("Joe", apple),
-				new Employee("Bob", sap),
-				new Employee("Joe", apple),
-				new Employee("Joe", amazon),
-				new Employee("Bob", sap),
-				new Employee("Joe", apple),
-				new Employee("Joe", sap),
-				new Employee("Joe", amazon),
-				new Employee("Joe", apple),
-				new Employee("Joe", microsoft));
-
-		getUniqueCompanies(employees).forEach(System.out::println);
 
 
-		/**
+
+
+
+		/** ============================================================================= */
+		/** ============================================================================= */
+		/** ============================================================================= */
+
+
+
+
+
+
+
+
+		/** 7.0 bonus
 		 * write a static function called testVarargs
 		 * that takes a string and a list of integers using only primitive type parameters
 		 * Using stream, add each of the int in the list and print it out
 		 * Should print "The sum is 21"
 		 * Can you call the Functional interface implemented earlier?
 		 */
-		testVarargs("The sum is ", 1, 2, 3, 4, 5, 6);
+
+//		final String theSumIsString = testVarargs("The sum is ", 1, 2, 3, 4, 5, 6);
+//
+//
+//		if(theSumIsString.equals(expectedString))
+//		{
+//			System.out.println(getCompletedMessage(7));
+//		}
+//		else
+//		{
+//			throw (new Exception(getErrorMessage(7)));
+//		}
 
 	}
+
+	/**
+	 * or write your function down here
+	 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	/**#################
+	  HELPER FUNCTIONS
+	 #################*/
+
+	static String sap = "SAP"; static String apple = "Apple"; static String microsoft = "Microsoft"; static String amazon = "Amazon";
+
+	private static String getErrorMessage(double number)
+	{
+		return "The answer given doesn't seem to be correct for "+number+"\n"
+				+ "Can you please try again";
+	}
+
+	private static String getCompletedMessage(double number)
+	{
+		return "Done "+number;
+	}
+	final static List<Integer> expected = List.of(2, 2, 32, 5234);
+	final static List<String> expectedList = List.of("MOON", "PLANET", "JUPITER", "SATURN", "ANDROMIDA", "NEPTUNE", "PLUTO");
+
+	final static List<String> expectedUniqueCompanies = List.of(sap, apple, microsoft, amazon);
+	final static String expectedString = "The sum is 21";
 
 }
 
